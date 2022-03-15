67b2c470b1e5f8a70da081441298889b7577aa14
gd8213 <56254591+gd8213@users.noreply.github.com>
2022-03-15 11:06:44 +0100

    Init

---
+++ Software/Visualization/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Visualization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btConnectPLC = new System.Windows.Forms.Button();
+            this.btWriteParameter = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btConnectPLC
+            // 
+            this.btConnectPLC.AutoSize = true;
+            this.btConnectPLC.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btConnectPLC.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btConnectPLC.Location = new System.Drawing.Point(3, 16);
+            this.btConnectPLC.Name = "btConnectPLC";
+            this.btConnectPLC.Size = new System.Drawing.Size(259, 23);
+            this.btConnectPLC.TabIndex = 0;
+            this.btConnectPLC.Text = "Connect to PLC";
+            this.btConnectPLC.UseVisualStyleBackColor = true;
+            this.btConnectPLC.Click += new System.EventHandler(this.btConnectPLC_Click);
+            // 
+            // btWriteParameter
+            // 
+            this.btWriteParameter.AutoSize = true;
+            this.btWriteParameter.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btWriteParameter.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btWriteParameter.Location = new System.Drawing.Point(3, 16);
+            this.btWriteParameter.Name = "btWriteParameter";
+            this.btWriteParameter.Size = new System.Drawing.Size(259, 23);
+            this.btWriteParameter.TabIndex = 1;
+            this.btWriteParameter.Text = "Write Parameter to Inventor";
+            this.btWriteParameter.UseVisualStyleBackColor = true;
+            this.btWriteParameter.Click += new System.EventHandler(this.btWriteParameter_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.btConnectPLC);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(265, 42);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.AutoSize = true;
+            this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox2.Controls.Add(this.btWriteParameter);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(0, 42);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(265, 42);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 270);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btConnectPLC;
+        private System.Windows.Forms.Button btWriteParameter;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Software/Visualization/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Inventor;
+using System.Runtime.InteropServices;
+
+namespace Visualization
+{
+    public partial class Form1 : Form
+    {
+        Inventor.Application _invApp = default(Inventor.Application);
+        Inventor.AssemblyDocument _assDoc = default(Inventor.AssemblyDocument);
+        Inventor.UserParameters _userParameters = default(Inventor.UserParameters);
+        bool _started = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Get Inventor App
+            try {
+                _invApp = (Inventor.Application)Marshal.GetActiveObject("Inventor.Application");
+            } catch (Exception ex) {
+                MessageBox.Show("Start inventor, open an assembly and try it again");
+                /*       MessageBox.Show(ex.ToString());
+
+                       // Start Inventor if not open
+                       try {
+                           Type invAppType = Type.GetTypeFromProgID("Inventor.Application");
+
+                           _invApp = (Inventor.Application)System.Activator.CreateInstance(invAppType);
+                           _invApp.Visible = true;
+
+                           //Note: if the Inventor session is left running after this
+                           //form is closed, there will still an be and Inventor.exe 
+                           //running. We will use this Boolean to test in Form1.Designer.cs 
+                           //in the dispose method whether or not the Inventor App should
+                           //be shut down when the form is closed.
+                           _started = true;
+
+                       } catch (Exception ex2) {
+                           MessageBox.Show(ex2.ToString());
+                           MessageBox.Show("Unable to get or start Inventor");
+                       }
+                */
+            }
+
+
+        }
+
+        private void btConnectPLC_Click(object sender, EventArgs e)
+        {
+            ConnectToADS();
+        }
+
+        private void btWriteParameter_Click(object sender, EventArgs e)
+        {
+            WriteParameterToInventor();
+        }
+
+
+        private void WriteParameterToInventor()
+        {
+            if (CheckInventorDocument() == false) return;
+
+            // Write parameters
+            foreach (Inventor.UserParameter parameter in _userParameters)
+            {
+                parameter.Value = 12;
+                parameter.Expression = "1";
+                string name = parameter.Name;
+            }
+
+            // Update Assembly with new Parameter values
+            _assDoc.Update();       
+        }
+
+        private bool CheckInventorDocument()
+        {
+            if (_invApp.Documents.Count == 0) return false;
+            if (_invApp.ActiveDocumentType != DocumentTypeEnum.kAssemblyDocumentObject) return false;
+
+            _assDoc = (Inventor.AssemblyDocument)_invApp.ActiveDocument;
+            _userParameters = _assDoc.ComponentDefinition.Parameters.UserParameters;
+
+            return true;
+        }
+
+        private bool ConnectToADS()
+        {
+
+            return true;
+        }
+
+
+
+    }
+}
